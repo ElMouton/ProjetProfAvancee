354f1e8db7440fdd14850241573d4b17e524bd6b
Julian Herter <herter4u@aig-ats-310-l15.fst.site.univ-lorraine.fr>
2021-11-10 18:17:41 +0100

    Ajout de personnage + mvment

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 
 #define MULTIPLICATEUR 7
 #define TAILLE_ECRAN_X 140 * MULTIPLICATEUR
-#define TAILLE_ECRAN_Y 49 * MULTIPLICATEUR
+#define TAILLE_ECRAN_Y 91 * MULTIPLICATEUR
 
 int main()
 {
@@ -19,6 +19,7 @@ int main()
     taille_fichier(fichier, &nbLig, &nbCol);
     tab = lire_fichier(fichier);
     
+    afficher_tab_2D(tab, nbLig, nbCol);
     //--------------------------------INITIALISATION SDL--------------------------------//
     SDL_Event evenement;
 
@@ -37,26 +38,53 @@ int main()
     SDL_Renderer* renderer = SDL_CreateRenderer(fenetre, -1, SDL_RENDERER_ACCELERATED);
 
     //--------------------------------TEXTURES--------------------------------//
+
+    //texture des blocks de la map
     SDL_Texture* block = charger_image("Ressource_projet/block.bmp", renderer);
+
+    int nbBlockW = 2;
     int blockW, blockH;
     SDL_QueryTexture(block, NULL, NULL, &blockW, &blockH);
 
-    blockW /= 2;
+    blockW /= nbBlockW;    //Division par le nombre de texture
 
-    SDL_Rect SrcBlocks[2], DestBlock;
-    for(int i = 0; i < 2; i++){
+    SDL_Rect SrcBlocks[nbBlockW], DestBlock;   //Séparation dans le fichier source des différents block
+    for(int i = 0; i < nbBlockW; i++){
         SrcBlocks[i].x = i * 7;
         SrcBlocks[i].y = 0;
         SrcBlocks[i].w = blockW;
         SrcBlocks[i].h = blockH;
     }
 
+    //texture du personnage
+    Uint8 r = 255, g = 255, b = 255;
+    SDL_Texture* perso = charger_image_transparente("Ressource_projet/skin_player.bmp", renderer, r, g, b);
+
+    int nbPersoW = 3, nbPersoH = 2;
+    int persoW, persoH;
+    SDL_QueryTexture(perso, NULL, NULL, &persoW, &persoH);
+
+    persoW /= nbPersoW;
+    persoH /= nbPersoH;
+
+    SDL_Rect SrcPerso, DestPerso;
+    SrcPerso.x = 0;
+    SrcPerso.y = 0;
+    SrcPerso.w = persoW;
+    SrcPerso.h = persoH;
+
+    DestPerso.x = 7*MULTIPLICATEUR;
+    DestPerso.y = 7*MULTIPLICATEUR;
+    DestPerso.w = persoW*MULTIPLICATEUR;
+    DestPerso.h = persoW*MULTIPLICATEUR;
+
     //--------------------------------BOUCLE DE JEU--------------------------------//
     bool terminer = false;
 
     while(!terminer){
         SDL_RenderClear(renderer);
-
+        
+        //Création et affichage de la map
         for(int i = 0; i < nbLig; i ++){
             for(int j = 0; j < nbCol; j++){
                 DestBlock.x = j*7*MULTIPLICATEUR;
@@ -64,15 +92,24 @@ int main()
                 DestBlock.w = blockW*MULTIPLICATEUR;
                 DestBlock.h = blockH*MULTIPLICATEUR;
 
-                if(tab[i][j] == '0'){
-                    SDL_RenderCopy(renderer, block, &SrcBlocks[1], &DestBlock);
-                }
-                if(tab[i][j] == '1'){
+                switch (tab[i][j]){
+                case '1':
                     SDL_RenderCopy(renderer, block, &SrcBlocks[0], &DestBlock);
+                    break;
+                
+                default:
+                    SDL_RenderCopy(renderer, block, &SrcBlocks[1], &DestBlock);
+                    break;
                 }
             }
         }
 
+        //Affichage du personnage
+        SDL_RenderCopy(renderer, perso, &SrcPerso, &DestPerso);
+        
+        
+
+        //Evenement du clavier
         SDL_PollEvent(&evenement);
         switch (evenement.type){
         case SDL_QUIT:
@@ -84,7 +121,23 @@ int main()
             case SDLK_ESCAPE:
                 terminer = true;
                 break;
+
+            case SDLK_UP:
+                    DestPerso.y -= 1;
+                break;
+
+            case SDLK_DOWN:
+                DestPerso.y += 1;
+                break;
+
+            case SDLK_LEFT:
+                DestPerso.x -= 1;
+                break;
             
+            case SDLK_RIGHT:
+                DestPerso.x += 1;
+                break;
+
             default:
                 break;
             }
@@ -92,8 +145,12 @@ int main()
         default:
             break;
         }
+
+        //Affichage des éléments 
         SDL_RenderPresent(renderer);
     }
+
+
 
     SDL_Quit();
     return 0;
